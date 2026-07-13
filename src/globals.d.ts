d0acf5ec4954aa77147bc27b200da3a502ab4214
yuofi <pavel.d.yulov@gmail.com>
2026-07-13 13:05:39 +0300

    temporary deleted all

--- src/globals.d.ts
+++
@@ -1,6 +0,0 @@
-declare module 'swiper/css';
-declare module 'swiper/css/navigation';
-declare module 'swiper/css/pagination';
-declare module 'swiper/css/effect-fade';
-declare module 'swiper/css/effect-cube';
-declare module '*.css';
--- src/utils/env.ts
+++
@@ -1,7 +0,0 @@
-import {z} from "zod"
-
-export const zEnv = z.object({
-    VITE_BACKEND_URL: z.string().trim().min(1),
-});
-
-export const env = zEnv.parse(import.meta.env);
